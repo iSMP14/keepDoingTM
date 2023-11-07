33f83c791e94508ee88f7c27ee38e9566530008f
iSMP <javiergcartes14@gmail.com>
2023-11-07 15:38:07 -0300

    Drag and Drop implementados

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 document.getElementById("myButton").onclick = myFunction;
 
 let idCounter = 0;
-
 function myFunction(event) {
   event.preventDefault();
   idCounter++;
   const taskId = `div${idCounter}`;
   const taskNameValue = document.getElementById("taskName").value;
   const taskDescValue = document.getElementById("taskDescription").value;
-  console.log(taskNameValue, taskDescValue);
   newTaskElement(taskId, taskNameValue, taskDescValue);
 }
 
@@ -16,11 +14,11 @@ function newTaskElement(taskId, taskName, taskDescription) {
   const pItem = document.createElement("div");
   pItem.id = taskId;
   pItem.classList.add("task_style");
-  const node = document.createTextNode(`${taskName} : ${taskDescription}`);
+  pItem.draggable = true;
+  const node = document.createTextNode(`${taskName}\n${taskDescription}`);
   pItem.appendChild(node);
-
   const element = document.getElementById("pending");
-  const child = document.getElementById("div1");
+  const child = document.getElementById("id");
   element.insertBefore(pItem, child);
 }
 
@@ -30,3 +28,29 @@ function addTaskB() {
   const addTask = document.getElementById("form");
   addTask.classList.toggle("add_task--active");
 }
+
+let dragged = null;
+
+const elements = document.querySelectorAll("#pending, #inProgress, #done");
+
+elements.forEach((element) => {
+  element.addEventListener("dragstart", (event) => {
+    // store a ref. on the dragged elem
+    dragged = event.target;
+  });
+
+  element.addEventListener("dragover", (event) => {
+    // prevent default to allow drop
+    event.preventDefault();
+  });
+
+  element.addEventListener("drop", (event) => {
+    // prevent default action (open as link for some elements)
+    event.preventDefault();
+    // move dragged element to the selected drop target
+    if (event.target.className === "task_element") {
+      dragged.parentNode.removeChild(dragged);
+      event.target.appendChild(dragged);
+    }
+  });
+});
